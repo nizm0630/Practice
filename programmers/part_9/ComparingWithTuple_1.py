d3445e491387cc733640aff092032b1ac8ea1549
nizm0630 <50166007+nizm0630@users.noreply.github.com>
2022-03-24 20:32:28 +0900

    Update ComparingWithTuple_1.py

--- programmers/part_9/ComparingWithTuple_1.py
+++ programmers/part_9/ComparingWithTuple_1.py
@@ -1,3 +1,6 @@
+# [check_and_clear]는 딕셔너리 타입의 box를 매개변수로 받음
+# box에 "불량품"이라는 이름표가 있으면 box 전체를 빈 딕셔너리로 만들어 버리고, 불량품이 없으면 box를 그대로 두는 함수
+
 def check_and_clear(box):
     if "불량품" in box:
         box.clear()
