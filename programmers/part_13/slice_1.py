7583eacd71abf5b5e9beda4714efcdc2229bc864
nizm0630 <50166007+nizm0630@users.noreply.github.com>
2022-03-24 21:33:40 +0900

    Create slice_1.py

---
+++ programmers/part_13/slice_1.py
@@ -0,0 +1,18 @@
+rainbow = ["빨", "주", "노", "초", "파", "남", "보"]
+
+# red_colors가 ["빨", "주", "노"]의 값을 가지도록 rainbow를 slice
+red_colors = rainbow[   ]
+
+#blue_colors가 ["파", "남", "보"]의 값을 가지도록 rainbow를 slice
+blue_colors = rainbow[  ]
+
+print("red_colors의 값 : {}".format(red_colors))
+print("blue_colors의 값 : {}".format(blue_colors))
+
+'''
+text = "hello world"
+text[1:5]         # ello          1번째부터 5번째 전까지
+text[3:]          # lo world      3번째부터 끝까지
+text[:3]          # hel           처음부터 3번째까지
+text[:]           # hello world   처음부터 끝까지
+'''
