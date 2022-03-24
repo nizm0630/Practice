f923522f0432a256b1ac35c0691de6bebe83fea0
nizm0630 <50166007+nizm0630@users.noreply.github.com>
2022-03-24 16:32:07 +0900

    Create conditional_2.py

---
+++ programmers/part_3/conditional_2.py
@@ -0,0 +1,9 @@
+# 어떤 숫자가 3의 배수인지 알아보기 위해 3으로 나눈 나머지가 0인지 확인하는 조건문
+
+number = 15
+if number % 3 == 0: #number가 3의 배수인지 확인합니다.
+    print("{}는 3의 배수입니다.".format(number))#이 코드는 실행됩니다.
+
+number = 16
+if number % 3 == 0: #number가 3의 배수인지 확인합니다.
+    print("{}는 3의 배수입니다.".format(number))#이 코드는 실행되지 않습니다.
