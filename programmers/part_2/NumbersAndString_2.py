75d4f8ae7f062a0092ffa4a404709fec57252cf5
nizm0630 <50166007+nizm0630@users.noreply.github.com>
2022-03-24 16:21:30 +0900

    Create NumbersAndString_2.py

---
+++ programmers/part_2/NumbersAndString_2.py
@@ -0,0 +1,7 @@
+# 두 변수 birth_year, birth_date를 활용하여 year_and_date 문자열 생성
+
+birth_year='1985'
+birth_date='0502'
+year_and_date =birth_year + birth_date
+
+print("year_and_date : {}".format(year_and_date))
