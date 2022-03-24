c12466c86811b851b65f9d1a7fa986b7e7f5bbb3
nizm0630 <50166007+nizm0630@users.noreply.github.com>
2022-03-24 16:20:09 +0900

    Create NumbersAndString.py

---
+++ programmers/part_2/NumbersAndString.py
@@ -0,0 +1,23 @@
+a=33
+b=3
+
+summation = a+b
+# a와 b의 합
+
+multiply =  a*b
+# a와 b의 곱
+
+divide =  a/b
+# a와 b의 몫
+
+remainder =  a%b
+# a를 b로 나눈 나머지
+
+power = a**b
+# a의 b승
+
+print("summation은 {}입니다.".format(summation))
+print("multiply는 {}입니다.".format(multiply))
+print("divide는 {}입니다.".format(divide))
+print("remainder는 {}입니다.".format(remainder))
+print("power는 {}입니다.".format(power))
