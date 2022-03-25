25e8d27f41cc70fb640c02805d019fbd82bf44d4
nizm0630 <50166007+nizm0630@users.noreply.github.com>
2022-03-25 13:15:43 +0900

    Create HandlingDataType.py

---
+++ programmers/part_14/HandlingDataType.py
@@ -0,0 +1,11 @@
+my_list = [1, 2, 3]
+my_dict = {"풀": 800, "색연필": 3000}
+my_tuple = (1, 2, 3)
+number = 10
+real_number = 3.141592
+
+print(type(my_list))
+print(type(my_dict))
+print(type(my_tuple))
+print(type(number))
+print(type(real_number))
