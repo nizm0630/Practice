89e93a004404f419e360b0d37ee6af15ff041155
nizm0630 <50166007+nizm0630@users.noreply.github.com>
2022-03-25 13:48:15 +0900

    Create MyException.py

---
+++ programmers/part_15/MyException.py
@@ -0,0 +1,17 @@
+# 예외처리
+class MyException(Exception):
+
+shops = {
+    "송일문방구": {"가위": 500, "크레파스": 3000},
+    "알파문구": {"풀": 800, "도화지": 300, "A4용지": 8000},
+    "다이소": {"풀": 500, "목공본드": 2000, "화분": 3000}
+}
+
+try:
+    for shop, products in shops.items():
+        for product, price in products.items():
+            if product == '풀':
+                print("{}: {}원".format(shop, price))
+                raise MyException
+except MyException:
+    print("풀을 찾았습니다.")
